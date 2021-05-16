0e5afe8ce8216d38cbaa36bbcee685e189edd75b
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-05-16 00:54:50 +0000

    centered text on mobile

--- components/Sections/Presentation.js
+++ components/Sections/Presentation.js
@@ -17,6 +17,11 @@ const Text = styled.div`
     margin-bottom: 0;
     color: #fff;
     padding: ${props => props.isMobile ? '2em' : '4em'};
+
+    text-align: center;
+    @media (min-width: 768px) {
+        text-align: left;
+    }
     h1 {
         font-family: 'Montserrat', sans-serif;
     }
@@ -66,11 +71,12 @@ const Presentation = () => {
                         </Col>
                         <Col sm={12} md={8}>
                             <Paragraph>
-                                <p>Après une longue histoire à succès, GhisaMestieri change de nom pour marquer une nouvelle ère: [] est né.</p>
+                                <p>Après une longue histoire à succès, X change de nom pour marquer une nouvelle ère: [] est né.</p>
                                 <p>Une entreprise marocaine d'éclairage public avec une philosophie précise: offrir à ses clients une gamme de solutions d'éclairage total avec des solutions adaptées à toutes les applications.</p>
                                 <p>Au fil du temps, nous avons développé une large gamme de produits d'éclairage historiques ou contemporains, adaptés à tout contexte urbain, périurbain ou architectural, et nous proposons des produits pour le mobilier urbain au design moderne ou classique.</p>
                                 <p>Nous proposons une série de solutions à haute valeur esthétique et design, parfaitement combinées avec la meilleure technologie LED qui a totalement changé la façon de penser et de concevoir l'éclairage.</p>
                                 <p>Nous nous sommes engagés dans une «voie verte» et avons développé une conscience environnementale visant à réduire la consommation d'énergie et les émissions de CO2.</p>
+                                <p>*Contenu Exemple</p>
                             </Paragraph>
                         </Col> 
                     </Row>
