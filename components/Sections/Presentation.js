a37ae4e8341401048e8c84c39c121b787121193e
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-05-31 11:38:25 +0100

    added animation to presentation component

--- components/Sections/Presentation.js
+++ components/Sections/Presentation.js
@@ -4,6 +4,11 @@ import { DeviceSize } from '../Navbar/responsive'
 import { useMediaQuery } from 'react-responsive'
 import { Col, Container, Row } from 'react-bootstrap';
 
+import { motion } from 'framer-motion'
+import { InView } from 'react-intersection-observer'
+
+
+
 
 const PresentationSection = styled.section`
     position: relative;
@@ -60,25 +65,43 @@ const Presentation = () => {
                 <Container>
                     <Row>
                         <Col sm={12} md={4}>
-                            <Text isMobile={isMobile}>
-                                <h1 className="big">
-                                    Solution
-                                    <br />
-                                    d'éclairage
-                                </h1>
-                                <h1 className="verde">complète</h1>
-                            </Text>
+                            <InView threshold={0.65}>
+                                {({ ref, inView }) => (
+                                    <motion.div ref={ref} initial={{ opacity: 0, x: -50 }}
+                                        animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: -50 }}
+                                        transition={{ duration: 0.8 }}>
+                                        <Text isMobile={isMobile}>
+                                            <h1 className="big">
+                                                Solution
+                                                <br />
+                                                d'éclairage
+                                            </h1>
+                                            <h1 className="verde">complète</h1>
+                                        </Text>
+                                    </motion.div>
+                                )}
+                            </InView>
+
                         </Col>
                         <Col sm={12} md={8}>
-                            <Paragraph>
-                                <p>Après une longue histoire à succès, X change de nom pour marquer une nouvelle ère: [] est né.</p>
-                                <p>Une entreprise marocaine d'éclairage public avec une philosophie précise: offrir à ses clients une gamme de solutions d'éclairage total avec des solutions adaptées à toutes les applications.</p>
-                                <p>Au fil du temps, nous avons développé une large gamme de produits d'éclairage historiques ou contemporains, adaptés à tout contexte urbain, périurbain ou architectural, et nous proposons des produits pour le mobilier urbain au design moderne ou classique.</p>
-                                <p>Nous proposons une série de solutions à haute valeur esthétique et design, parfaitement combinées avec la meilleure technologie LED qui a totalement changé la façon de penser et de concevoir l'éclairage.</p>
-                                <p>Nous nous sommes engagés dans une «voie verte» et avons développé une conscience environnementale visant à réduire la consommation d'énergie et les émissions de CO2.</p>
-                                <p>*Contenu Exemple</p>
-                            </Paragraph>
-                        </Col> 
+                            <InView threshold={0.65}>
+                                {({ ref, inView }) => (
+                                    <motion.div ref={ref} initial={{ opacity: 0, x: 50 }}
+                                        animate={inView ? { opacity: 1, x: 0 } : { opacity: 0, x: 50 }}
+                                        transition={{ duration: 0.8 }}>
+                                        <Paragraph>
+                                            <p>Après une longue histoire à succès, X change de nom pour marquer une nouvelle ère: [] est né.</p>
+                                            <p>Une entreprise marocaine d'éclairage public avec une philosophie précise: offrir à ses clients une gamme de solutions d'éclairage total avec des solutions adaptées à toutes les applications.</p>
+                                            <p>Au fil du temps, nous avons développé une large gamme de produits d'éclairage historiques ou contemporains, adaptés à tout contexte urbain, périurbain ou architectural, et nous proposons des produits pour le mobilier urbain au design moderne ou classique.</p>
+                                            <p>Nous proposons une série de solutions à haute valeur esthétique et design, parfaitement combinées avec la meilleure technologie LED qui a totalement changé la façon de penser et de concevoir l'éclairage.</p>
+                                            <p>Nous nous sommes engagés dans une «voie verte» et avons développé une conscience environnementale visant à réduire la consommation d'énergie et les émissions de CO2.</p>
+                                            <p>*Contenu Exemple</p>
+                                        </Paragraph>
+                                    </motion.div>
+                                )}
+                            </InView>
+
+                        </Col>
                     </Row>
                 </Container>
 
