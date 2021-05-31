e7ee1ebd05211cd72f72d59ebc154eb317b7f07c
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-05-31 16:55:47 +0100

    added about cards

--- pages/about.js
+++ pages/about.js
@@ -4,17 +4,74 @@ import { SectionHeader } from "../components/SectionHeader";
 import { Text } from '../components/General';
 import styled from 'styled-components';
 import Contact from '../components/Contact'
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 import AutoContainer from "../components/AutoContainer";
+import AboutCard from "../components/CategoryCard/AboutCard";
+
+import { motion } from 'framer-motion';
+import { InView } from 'react-intersection-observer';
 
 
 const StyledText = styled(Text)`
     text-align: center;
     margin-bottom: 3em;
 `
+const CardSection = styled.div`
+    width: 100%;
+    background-color: #000;
+    z-index: -1999;
+`;
 
 
 export default function AboutPage() {
+
+    const aboutData = [
+        {
+            id: 1,
+            title: 'Eclairage Public et Routiers',
+            imgUrl: 'img/showcase-1.jpeg',
+            description: `GMR ENLIGHTS propose une large sélection de luminaires et de poteaux à
+           LED pour des applications fonctionnelles telles que les autoroutes et les routes interurbaines.
+           Le haut niveau technologique des solutions optiques et mécaniques adoptées pour l’éclairage public
+           et routier permet d’atteindre les résultats les plus performants, en termes d’économie d’énergie
+           et de confort visuel,
+           et de simplifier les opérations d’installation et d’entretien.`
+        },
+        {
+            id: 2,
+            title: 'Eclairage Public et Routiers',
+            imgUrl: 'img/showcase-1.jpeg',
+            description: `GMR ENLIGHTS propose une large sélection de luminaires et de poteaux à
+            LED pour des applications fonctionnelles telles que les autoroutes et les routes interurbaines.
+            Le haut niveau technologique des solutions optiques et mécaniques adoptées pour l’éclairage public
+            et routier permet d’atteindre les résultats les plus performants, en termes d’économie d’énergie
+            et de confort visuel,
+            et de simplifier les opérations d’installation et d’entretien.`
+        },
+        {
+            id: 3,
+            title: 'Eclairage Public et Routiers',
+            imgUrl: 'img/showcase-1.jpeg',
+            description: `GMR ENLIGHTS propose une large sélection de luminaires et de poteaux à
+            LED pour des applications fonctionnelles telles que les autoroutes et les routes interurbaines.
+            Le haut niveau technologique des solutions optiques et mécaniques adoptées pour l’éclairage public
+            et routier permet d’atteindre les résultats les plus performants, en termes d’économie d’énergie
+            et de confort visuel,
+            et de simplifier les opérations d’installation et d’entretien.`
+        },
+        {
+            id: 4,
+            title: 'Eclairage Public et Routiers',
+            imgUrl: 'img/showcase-1.jpeg',
+            description: `GMR ENLIGHTS propose une large sélection de luminaires et de poteaux à
+            LED pour des applications fonctionnelles telles que les autoroutes et les routes interurbaines.
+            Le haut niveau technologique des solutions optiques et mécaniques adoptées pour l’éclairage public
+            et routier permet d’atteindre les résultats les plus performants, en termes d’économie d’énergie
+            et de confort visuel,
+            et de simplifier les opérations d’installation et d’entretien.`
+        }
+    ]
+
     return (
         <>
 
@@ -41,6 +98,30 @@ export default function AboutPage() {
                         </Col>
                     </Row>
                 </AutoContainer>
+                <InView threshold={0.25}>
+                    {({ ref, inView }) => (
+                        <motion.div ref={ref} initial={{ opacity: 0 }}
+                            animate={inView ? { opacity: 1 } : { opacity: 0 }}
+                            transition={{ duration: 0.8 }}>
+                            <CardSection>
+
+                                <Container>
+                                    <Row>
+                                        {aboutData?.map((card) => (
+
+                                            <Col key={card.id} md={6} lg={6} sm={12}>
+                                                <AboutCard title={card.title} imgUrl={card.imgUrl} description={card.description} />
+                                            </Col>
+
+
+                                        ))}
+                                    </Row>
+                                </Container>
+                            </CardSection>
+                        </motion.div>
+                    )}
+                </InView>
+
                 <Contact />
 
             </Layout>
