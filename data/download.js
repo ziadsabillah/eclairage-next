8a6c6764efb0db779c28348aa2d6d6483aeff739
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-02 15:39:18 +0100

    added downloads page

---
+++ data/download.js
@@ -0,0 +1,19 @@
+const download = [
+    {
+        id: 0,
+        title: "Profile de l'entreprise",
+        imgUrl: '/img/company-profile.jpeg',
+    },
+    {
+        id: 1,
+        title: "Element Telechargeable ",
+        imgUrl: '/img/company-profile.jpeg',
+    },
+    {
+        id: 2,
+        title: "Element Telechargeable 2",
+        imgUrl: '/img/company-profile.jpeg',
+    }
+]
+
+export default download;
