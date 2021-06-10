e6218a8d5018ce44624350ba1f8990304c2072a0
ZiaDoox <ziadoox.sabillah@gmail.com>
2021-06-10 17:39:04 +0100

    finished faq

--- components/Faq.js
+++ components/Faq.js
@@ -1,6 +1,7 @@
 import { Accordion, Container, Card, Button } from 'react-bootstrap';
+import { AiOutlineArrowDown } from 'react-icons/ai';
 import styled from 'styled-components';
-import {SectionHeader } from '../components/SectionHeader'
+import { SectionHeader } from '../components/SectionHeader'
 
 
 const FAQSection = styled.div`
@@ -14,36 +15,54 @@ const CustomAccordion = styled(Accordion)`
     a {
         text-decoration: none;
     }
-    button {
+    button, button:hover, button:focus, button.active {
         text-decoration: none;
+        color: grey;
+
     }
 `
 
-const Faq = ({data}) => {
+const CustomCard = styled(Card)`
+    border: none;
+    border-bottom: 1px solid lightgrey;
+    margin-bottom: 10px;
+
+    
+    .card-header {
+        background: #fff !important;
+        border: none;
+
+    }
+   
+`;
+
+const Faq = ({ data }) => {
     return (
         <>
 
-        <Container>
-            <SectionHeader>
-                <h2 style={{ marginTop: '1em' }}>Questions Fréquemment Posées</h2>
-            </SectionHeader>
-            <FAQSection>
-                <Accordion defaultActiveKey="0">
-                    {data?.map(item => (
-                        <Card>
-                            <Card.Header>
-                                <Accordion.Toggle eventKey={item.id} as={Button} variant="link">
-                                    {item.question}
-                                </Accordion.Toggle>
-                            </Card.Header>
-                            <Accordion.Collapse eventKey={item.id}>
-                                <Card.Body>{item.answer}</Card.Body>
-                            </Accordion.Collapse>
-                        </Card>
-                    ))}
-                </Accordion>
-            </FAQSection>
-        </Container>
+            <Container>
+                <SectionHeader>
+                    <h2 style={{ marginTop: '1em' }}>FAQ</h2>
+                </SectionHeader>
+                <FAQSection>
+                    <CustomAccordion defaultActiveKey="0">
+                        {data?.map(item => (
+                            <CustomCard>
+                                <Card.Header>
+                                    <Accordion.Toggle eventKey={item.id} as={Button} variant="link">
+                                        Q{item.id + '. ' +item.question}
+
+                                    </Accordion.Toggle>
+
+                                </Card.Header>
+                                <Accordion.Collapse eventKey={item.id}>
+                                    <Card.Body>{item.answer}</Card.Body>
+                                </Accordion.Collapse>
+                            </CustomCard>
+                        ))}
+                    </CustomAccordion>
+                </FAQSection>
+            </Container>
 
 
         </>
--- data/faq.js
+++ data/faq.js
@@ -1,19 +1,23 @@
 const faq = [
-   
     {
         id: 1,
         question: 'Question 1',
-        answer: 'test'
+        answer: 'Reponse 1'
     },
     {
         id: 2,
-        question: 'test',
-        answer: 'test'
+        question: 'Question 2',
+        answer: 'Reponse 2'
     },
     {
         id: 3,
-        question: 'Test 2',
-        answer: 'Test 2 answer'
+        question: 'Question 3',
+        answer: 'Reponse 3'
+    },
+    {
+        id: 4,
+        question: 'Question 4',
+        answer: 'Reponse 4'
     }
 ]
 
